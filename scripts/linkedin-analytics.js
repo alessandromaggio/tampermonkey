58dc95eb1ac6d5dc62d944c54156124c07f232bc
Alessandro Maggio <alessandro.maggio96@gmail.com>
2026-02-02 12:38:14 +0000

    Added additional metadat

--- scripts/linkedin-analytics.js
+++ scripts/linkedin-analytics.js
@@ -1,6 +1,10 @@
 // ==UserScript==
-// @name         LinkedIn Analytics by Alessandro Maggio
+// @name         LinkedIn Analytics
 // @match        https://linkedin.com/*
+// @namespace    http://tampermonkey.net/
+// @version      2026-02-02
+// @description  A Tampermonkey script for LinkedIn analytics
+// @author       Alessandro Maggio
 // @updateURL    https://raw.githubusercontent.com/alessandromaggio/tampermonkey/refs/heads/main/scripts/linkedin-analytics.js
 // @downloadURL  https://raw.githubusercontent.com/alessandromaggio/tampermonkey/refs/heads/main/scripts/linkedin-analytics.js
 // ==/UserScript==
