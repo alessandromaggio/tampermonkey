05b2946852454078a9ada28ca509740c785499e2
Alessandro Maggio <alessandro.maggio96@gmail.com>
2026-02-02 12:32:25 +0000

    feat: Created initial script

---
+++ scripts/linkedin-analytics.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name         My Script
+// @match        https://example.com/*
+// @updateURL    https://raw.githubusercontent.com/you/repo/main/myscript.user.js
+// @downloadURL  https://raw.githubusercontent.com/you/repo/main/myscript.user.js
+// ==/UserScript==
+console.log("My Script is running!");
